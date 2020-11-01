4917b62f315ed23dd7ae631d827bd0c009540b84
Paweł Wegner <pawel.wegner95@gmail.com>
2020-11-01 12:44:27 +0100

    curl_http: sort methods in .cc file.

--- src/coro/http/curl_http.cc
+++ src/coro/http/curl_http.cc
@@ -50,79 +50,6 @@ std::string TrimWhitespace(std::string_view str) {
 
 }  // namespace
 
-void CurlHttp::ProcessEvents(CURLM* multi_handle) {
-  CURLMsg* message;
-  do {
-    int message_count;
-    message = curl_multi_info_read(multi_handle, &message_count);
-    if (message && message->msg == CURLMSG_DONE) {
-      CURL* handle = message->easy_handle;
-      CurlHandle* curl_handle;
-      Check(curl_easy_getinfo(handle, CURLINFO_PRIVATE, &curl_handle));
-      if (std::holds_alternative<CurlHttpOperation*>(curl_handle->owner_)) {
-        auto operation = std::get<CurlHttpOperation*>(curl_handle->owner_);
-        if (message->data.result == CURLE_OK) {
-          long response_code;
-          Check(curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE,
-                                  &response_code));
-          operation->response_.status = static_cast<int>(response_code);
-        } else {
-          operation->exception_ptr_ = std::make_exception_ptr(HttpException(
-              message->data.result, curl_easy_strerror(message->data.result)));
-        }
-        operation->resume();
-      }
-    }
-  } while (message != nullptr);
-}
-
-void CurlHttp::SocketEvent(evutil_socket_t fd, short event, void* handle) {
-  int running_handles;
-  Check(
-      curl_multi_socket_action(handle, fd,
-                               ((event & EV_READ) ? CURL_CSELECT_IN : 0) |
-                                   ((event & EV_WRITE) ? CURL_CSELECT_OUT : 0),
-                               &running_handles));
-  ProcessEvents(handle);
-}
-
-int CurlHttp::SocketCallback(CURL*, curl_socket_t socket, int what, void* userp,
-                             void* socketp) {
-  auto http = reinterpret_cast<CurlHttp*>(userp);
-  if (what == CURL_POLL_REMOVE) {
-    auto data = reinterpret_cast<SocketData*>(socketp);
-    if (data) {
-      Check(event_del(&data->socket_event));
-      delete data;
-    }
-  } else {
-    auto data = reinterpret_cast<SocketData*>(socketp);
-    if (!data) {
-      data = new SocketData;
-      Check(curl_multi_assign(http->curl_handle_, socket, data));
-    } else {
-      Check(event_del(&data->socket_event));
-    }
-    Check(event_assign(&data->socket_event, http->event_loop_, socket,
-                       ((what & CURL_POLL_IN) ? EV_READ : 0) |
-                           ((what & CURL_POLL_OUT) ? EV_WRITE : 0) | EV_PERSIST,
-                       SocketEvent, http->curl_handle_));
-    Check(event_add(&data->socket_event, nullptr));
-  }
-  return 0;
-}
-
-int CurlHttp::TimerCallback(CURLM*, long timeout_ms, void* userp) {
-  auto http = reinterpret_cast<CurlHttp*>(userp);
-  if (timeout_ms == -1) {
-    Check(event_del(&http->timeout_event_));
-  } else {
-    timeval tv = {.tv_sec = timeout_ms / 1000, .tv_usec = timeout_ms % 1000};
-    Check(event_add(&http->timeout_event_, &tv));
-  }
-  return 0;
-}
-
 size_t CurlHandle::HeaderCallback(char* buffer, size_t size, size_t nitems,
                                   void* userdata) {
   auto handle = reinterpret_cast<CurlHandle*>(userdata);
@@ -248,6 +175,79 @@ CurlHttp::CurlHttp(event_base* event_loop)
 CurlHttp::~CurlHttp() {
   Check(event_del(&timeout_event_));
   Check(curl_multi_cleanup(curl_handle_));
+}
+
+void CurlHttp::ProcessEvents(CURLM* multi_handle) {
+  CURLMsg* message;
+  do {
+    int message_count;
+    message = curl_multi_info_read(multi_handle, &message_count);
+    if (message && message->msg == CURLMSG_DONE) {
+      CURL* handle = message->easy_handle;
+      CurlHandle* curl_handle;
+      Check(curl_easy_getinfo(handle, CURLINFO_PRIVATE, &curl_handle));
+      if (std::holds_alternative<CurlHttpOperation*>(curl_handle->owner_)) {
+        auto operation = std::get<CurlHttpOperation*>(curl_handle->owner_);
+        if (message->data.result == CURLE_OK) {
+          long response_code;
+          Check(curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE,
+                                  &response_code));
+          operation->response_.status = static_cast<int>(response_code);
+        } else {
+          operation->exception_ptr_ = std::make_exception_ptr(HttpException(
+              message->data.result, curl_easy_strerror(message->data.result)));
+        }
+        operation->resume();
+      }
+    }
+  } while (message != nullptr);
+}
+
+void CurlHttp::SocketEvent(evutil_socket_t fd, short event, void* handle) {
+  int running_handles;
+  Check(
+      curl_multi_socket_action(handle, fd,
+                               ((event & EV_READ) ? CURL_CSELECT_IN : 0) |
+                                   ((event & EV_WRITE) ? CURL_CSELECT_OUT : 0),
+                               &running_handles));
+  ProcessEvents(handle);
+}
+
+int CurlHttp::SocketCallback(CURL*, curl_socket_t socket, int what, void* userp,
+                             void* socketp) {
+  auto http = reinterpret_cast<CurlHttp*>(userp);
+  if (what == CURL_POLL_REMOVE) {
+    auto data = reinterpret_cast<SocketData*>(socketp);
+    if (data) {
+      Check(event_del(&data->socket_event));
+      delete data;
+    }
+  } else {
+    auto data = reinterpret_cast<SocketData*>(socketp);
+    if (!data) {
+      data = new SocketData;
+      Check(curl_multi_assign(http->curl_handle_, socket, data));
+    } else {
+      Check(event_del(&data->socket_event));
+    }
+    Check(event_assign(&data->socket_event, http->event_loop_, socket,
+                       ((what & CURL_POLL_IN) ? EV_READ : 0) |
+                           ((what & CURL_POLL_OUT) ? EV_WRITE : 0) | EV_PERSIST,
+                       SocketEvent, http->curl_handle_));
+    Check(event_add(&data->socket_event, nullptr));
+  }
+  return 0;
+}
+
+int CurlHttp::TimerCallback(CURLM*, long timeout_ms, void* userp) {
+  auto http = reinterpret_cast<CurlHttp*>(userp);
+  if (timeout_ms == -1) {
+    Check(event_del(&http->timeout_event_));
+  } else {
+    timeval tv = {.tv_sec = timeout_ms / 1000, .tv_usec = timeout_ms % 1000};
+    Check(event_add(&http->timeout_event_, &tv));
+  }
+  return 0;
 }
 
 HttpOperation CurlHttp::Fetch(Request&& request) {

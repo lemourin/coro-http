cbf10673f6718dd8f76a8db7c3a5d46b3fbf27ab
Paweł Wegner <pawel.wegner95@gmail.com>
2020-11-07 14:22:14 +0100

    wait_task: support interrupting.

--- src/coro/wait_task.cpp
+++ src/coro/wait_task.cpp
@@ -1,19 +1,18 @@
 #include "wait_task.h"
 
-#include <coro/stop_token.h>
-
 namespace coro {
 
 WaitTask::WaitTask(event_base *event_loop, int msec,
                    coro::stop_token &&stop_token)
-    : stop_token_(std::move(stop_token)) {
+    : stop_token_(std::move(stop_token)),
+      stop_callback_(stop_token_, OnCancel{this}) {
   timeval tv = {.tv_sec = msec / 1000, .tv_usec = msec % 1000 * 1000};
   event_assign(
       &event_, event_loop, -1, EV_TIMEOUT,
       [](evutil_socket_t, short, void *data) {
         auto task = reinterpret_cast<WaitTask *>(data);
         if (task->handle_) {
-          task->handle_.resume();
+          std::exchange(task->handle_, nullptr).resume();
         }
       },
       this);
@@ -25,6 +24,14 @@ WaitTask::~WaitTask() { event_del(&event_); }
 WaitTask Wait(event_base *event_loop, int msec,
               coro::stop_token &&stop_token) noexcept {
   return WaitTask(event_loop, msec, std::move(stop_token));
+}
+
+void WaitTask::OnCancel::operator()() const {
+  event_del(&task->event_);
+  if (task->handle_) {
+    task->interrupted_ = true;
+    std::exchange(task->handle_, nullptr).resume();
+  }
 }
 
 }  // namespace coro
--- src/coro/wait_task.h
+++ src/coro/wait_task.h
@@ -1,12 +1,20 @@
 #ifndef CORO_HTTP_WAIT_TASK_H
 #define CORO_HTTP_WAIT_TASK_H
 
-#include <coro/task.h>
+#include <coro/stop_callback.h>
 #include <coro/stop_token.h>
+#include <coro/task.h>
 #include <event2/event.h>
 #include <event2/event_struct.h>
 
 namespace coro {
+
+class InterruptedException : public std::exception {
+ public:
+  [[nodiscard]] const char* what() const noexcept override {
+    return "interrupted";
+  }
+};
 
 class WaitTask {
  public:
@@ -22,12 +30,23 @@ class WaitTask {
 
   bool await_ready() { return false; }
   void await_suspend(coroutine_handle<void> handle) { handle_ = handle; }
-  void await_resume() {}
+  void await_resume() {
+    if (interrupted_) {
+      throw InterruptedException();
+    }
+  }
 
  private:
+  struct OnCancel {
+    void operator()() const;
+    WaitTask* task;
+  };
+
   coroutine_handle<void> handle_;
   event event_;
   coro::stop_token stop_token_;
+  bool interrupted_ = false;
+  coro::stop_callback<OnCancel> stop_callback_;
 };
 
 WaitTask Wait(event_base* event_loop, int msec,

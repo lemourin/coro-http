5781d686d18c7d3f238903bfdaf962ca55b64cb9
Paweł Wegner <pawel.wegner95@gmail.com>
2020-12-25 15:42:06 +0100

    task: don't heap allocate when task returns value.

--- src/coro/task.h
+++ src/coro/task.h
@@ -17,70 +17,85 @@ class Task;
 template <typename T>
 class Task<T> {
  public:
-  using DataType =
-      std::variant<std::monostate, std::unique_ptr<T>, std::exception_ptr>;
-
   struct promise_type {
     struct final_awaitable {
-      bool await_ready() noexcept { return true; }
-      template <typename C>
-      auto await_suspend(C) noexcept {}
-      void await_resume() noexcept {
-        if (promise->continuation) {
-          promise->continuation.resume();
-        }
+      bool await_ready() noexcept { return false; }
+      auto await_suspend(stdx::coroutine_handle<promise_type> coro) noexcept {
+        return coro.promise().continuation;
       }
-
-      promise_type* promise;
+      void await_resume() noexcept {}
     };
 
+    promise_type() noexcept {}
+
+    ~promise_type() {
+      switch (type) {
+        case Type::kValue:
+          value.~T();
+          break;
+        case Type::kException:
+          exception.~exception_ptr();
+          break;
+        default:
+          break;
+      }
+    }
+
     Task get_return_object() {
-      return Task(stdx::coroutine_handle<promise_type>::from_promise(*this),
-                  value);
+      return Task(stdx::coroutine_handle<promise_type>::from_promise(*this));
     }
     stdx::suspend_never initial_suspend() { return {}; }
-    final_awaitable final_suspend() noexcept { return {this}; }
+    final_awaitable final_suspend() noexcept { return {}; }
     template <typename V>
     void return_value(V&& v) {
-      *value = std::make_unique<T>(std::forward<V>(v));
+      new (static_cast<void*>(std::addressof(value))) T(std::forward<V>(v));
+      type = Type::kValue;
     }
-    void unhandled_exception() { *value = std::current_exception(); }
+    void unhandled_exception() {
+      new (static_cast<void*>(std::addressof(exception)))
+          std::exception_ptr(std::current_exception());
+      type = Type::kException;
+    }
 
-    stdx::coroutine_handle<void> continuation;
-    std::shared_ptr<DataType> value = std::make_shared<DataType>();
+    stdx::coroutine_handle<void> continuation = coro::std_ns::noop_coroutine();
+    union {
+      T value;
+      std::exception_ptr exception;
+    };
+    enum class Type { kNone, kValue, kException } type = Type::kNone;
   };
 
   Task(const Task&) = delete;
-  Task(Task&& task) noexcept
-      : handle_(std::exchange(task.handle_, nullptr)),
-        value_(std::move(task.value_)) {}
+  Task(Task&& task) noexcept : handle_(std::exchange(task.handle_, nullptr)) {}
+  ~Task() {
+    if (handle_) {
+      handle_.destroy();
+    }
+  }
   Task& operator=(const Task&) = delete;
   Task& operator=(Task&& task) noexcept {
     handle_ = std::exchange(task.handle_, nullptr);
-    value_ = std::move(task.value_);
     return *this;
   }
 
   bool await_ready() {
-    return !std::holds_alternative<std::monostate>(*value_);
+    return handle_.promise().type != promise_type::Type::kNone;
   }
   void await_suspend(stdx::coroutine_handle<void> continuation) {
     handle_.promise().continuation = continuation;
   }
   T await_resume() {
-    if (std::holds_alternative<std::exception_ptr>(*value_)) {
-      std::rethrow_exception(std::get<std::exception_ptr>(*value_));
+    if (handle_.promise().type == promise_type::Type::kException) {
+      std::rethrow_exception(handle_.promise().exception);
     }
-    return std::move(*std::get<std::unique_ptr<T>>(*value_));
+    return std::move(handle_.promise().value);
   }
 
  private:
-  Task(stdx::coroutine_handle<promise_type> handle,
-       std::shared_ptr<DataType> value)
-      : handle_(handle), value_(std::move(value)) {}
+  explicit Task(stdx::coroutine_handle<promise_type> handle)
+      : handle_(handle) {}
 
   stdx::coroutine_handle<promise_type> handle_;
-  std::shared_ptr<DataType> value_;
 };
 
 template <>

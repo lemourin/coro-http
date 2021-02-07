2a1cf7cd1c85a304eea797a318ea8dfa4c756274
Paweł Wegner <pawel.wegner95@gmail.com>
2021-02-07 12:10:24 +0100

    raii_utils: don't require functor to be move assignable.

--- src/coro/util/raii_utils.h
+++ src/coro/util/raii_utils.h
@@ -5,30 +5,38 @@
 
 namespace coro::util {
 
+namespace internal {
+
+template <typename F>
+struct Guard {
+  explicit Guard(F func) : func(std::move(func)) {}
+  ~Guard() {
+    if (func) {
+      (*func)();
+    }
+  }
+  Guard(const Guard&) = delete;
+  template <typename Other>
+  Guard(Other&& other) noexcept {
+    func = std::move(other.func);
+    other.func = std::nullopt;
+  }
+  Guard& operator=(const Guard&) = delete;
+  template <typename Other>
+  Guard& operator=(Other&& other) noexcept {
+    func = std::move(other.func);
+    other.func = std::nullopt;
+    return *this;
+  }
+
+  std::optional<F> func;
+};
+
+}  // namespace internal
+
 template <typename F>
 auto AtScopeExit(F func) {
-  struct Guard {
-    explicit Guard(F func) : func(std::move(func)) {}
-    ~Guard() {
-      if (func) {
-        (*func)();
-      }
-    }
-    Guard(const Guard&) = delete;
-    Guard(Guard&& other) noexcept {
-      func = std::move(other.func);
-      other.func = std::nullopt;
-    }
-    Guard& operator=(const Guard&) = delete;
-    Guard& operator=(Guard&& other) noexcept {
-      func = std::move(other.func);
-      other.func = std::nullopt;
-      return *this;
-    }
-
-    std::optional<F> func;
-  };
-  return Guard(std::move(func));
+  return internal::Guard(std::move(func));
 }
 
 template <typename T, typename F>

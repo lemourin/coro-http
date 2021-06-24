23f9a4feb30f7405dfb13fe21678a74762cbc509
Paweł Wegner <pawel.wegner95@gmail.com>
2021-06-24 22:55:12 +0200

    cmake: correctly escape arguments in embed_resource.

--- cmake/embed_resource.cmake
+++ cmake/embed_resource.cmake
@@ -7,15 +7,16 @@ function(embed_resource)
         OUTPUT ${EMBED_OUTPUT}.cc
         COMMAND
             ${CMAKE_COMMAND}
-                "-DNAMESPACE=\"${EMBED_NAMESPACE}\""
-                "-DOUTPUT=\"${EMBED_OUTPUT}\""
-                "-DASSETS=\"${EMBED_INPUT}\""
-                "-DCMAKE_MODULE_PATH=\"${CMAKE_MODULE_PATH}\""
+                "-DNAMESPACE=${EMBED_NAMESPACE}"
+                "-DOUTPUT=${EMBED_OUTPUT}"
+                "-DASSETS=${EMBED_INPUT}"
+                "-DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}"
                 -P "${EMBED_RESOURCE_DIR}/embed_resource_impl.cmake"
         DEPENDS
             ${EMBED_INPUT}
         WORKING_DIRECTORY
             ${CMAKE_CURRENT_SOURCE_DIR}
+        VERBATIM
     )
     embed_resource_header(
         OUTPUT ${EMBED_OUTPUT}.h

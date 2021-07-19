04914758ffbac3c5f9f2ee201fb22e5d3d68cfdb
Paweł Wegner <pawel.wegner95@gmail.com>
2021-07-19 22:13:47 +0200

    thread_pool: parameterize with EventLoop.

--- src/coro/util/thread_pool.h
+++ src/coro/util/thread_pool.h
@@ -14,12 +14,31 @@
 
 namespace coro::util {
 
+void SetThreadName(std::string_view thread_name);
+
+template <typename EventLoop>
 class ThreadPool {
  public:
   explicit ThreadPool(
-      const EventLoop& event_loop,
-      unsigned int thread_count = std::thread::hardware_concurrency());
-  ~ThreadPool();
+      const EventLoop* event_loop,
+      unsigned int thread_count = std::thread::hardware_concurrency())
+      : event_loop_(event_loop) {
+    for (unsigned int i = 0; i < std::max<unsigned int>(thread_count, 2u);
+         i++) {
+      threads_.emplace_back([&] { Work(); });
+    }
+  }
+
+  ~ThreadPool() {
+    {
+      std::unique_lock lock(mutex_);
+      quit_ = true;
+      condition_variable_.notify_all();
+    }
+    for (auto& thread : threads_) {
+      thread.join();
+    }
+  }
 
   ThreadPool(const ThreadPool&) = delete;
   ThreadPool(ThreadPool&) = delete;
@@ -51,9 +70,46 @@ class ThreadPool {
   }
 
  private:
-  void Work();
-  Task<> SwitchToThreadLoop();
-  Task<> SwitchToEventLoop();
+  void Work() {
+    SetThreadName("coro-threadpool");
+    while (true) {
+      std::unique_lock lock(mutex_);
+      condition_variable_.wait(lock, [&] { return !tasks_.empty() || quit_; });
+      if (quit_ && tasks_.empty()) {
+        break;
+      }
+      auto coroutine = tasks_.back();
+      tasks_.pop_back();
+      lock.unlock();
+      coroutine.resume();
+    }
+  }
+
+  Task<> SwitchToThreadLoop() {
+    struct Awaiter {
+      bool await_ready() const { return false; }
+      void await_resume() {}
+      void await_suspend(stdx::coroutine_handle<void> continuation) {
+        std::unique_lock lock(thread_pool->mutex_);
+        thread_pool->tasks_.emplace_back(continuation);
+        thread_pool->condition_variable_.notify_one();
+      }
+      ThreadPool* thread_pool;
+    };
+    co_await Awaiter{this};
+  }
+
+  Task<> SwitchToEventLoop() {
+    struct Awaiter {
+      bool await_ready() const { return false; }
+      void await_resume() {}
+      void await_suspend(stdx::coroutine_handle<void> continuation) {
+        event_loop->RunOnEventLoop([=]() mutable { continuation.resume(); });
+      }
+      const EventLoop* event_loop;
+    };
+    co_await Awaiter{event_loop_};
+  }
 
   std::vector<stdx::coroutine_handle<void>> tasks_;
   std::vector<std::thread> threads_;
@@ -62,8 +118,6 @@ class ThreadPool {
   std::mutex mutex_;
   const EventLoop* event_loop_;
 };
-
-void SetThreadName(std::string_view thread_name);
 
 }  // namespace coro::util
 

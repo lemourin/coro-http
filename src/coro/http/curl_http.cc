5a09e97ce1777ba392cb0928eea46b480d0439b5
Paweł Wegner <pawel.wegner95@gmail.com>
2022-06-11 20:25:42 +0200

    curl_http: hide curl and libevent references.

--- src/coro/http/curl_http.cc
+++ src/coro/http/curl_http.cc
@@ -1,5 +1,9 @@
 #include "coro/http/curl_http.h"
 
+#include <curl/curl.h>
+#include <event2/event.h>
+#include <event2/event_struct.h>
+
 #include <memory>
 #include <sstream>
 #include <string>
@@ -76,7 +80,146 @@ void MoveAssignEvent(event* target, event* source, void* userdata) {
   MoveEvent(target, source, userdata);
 }
 
-}  // namespace
+class CurlHttpImpl;
+class CurlHttpOperation;
+class CurlHttpBodyGenerator;
+
+class CurlHandle {
+ public:
+  CurlHandle(CurlHandle&&) = default;
+  CurlHandle& operator=(CurlHandle&&) = default;
+
+  ~CurlHandle();
+
+ private:
+  template <typename Owner>
+  CurlHandle(CURLM* http, event_base* event_loop, Request<>,
+             const std::string* cache_path, stdx::stop_token, Owner*);
+
+  template <typename NewOwner>
+  CurlHandle(CurlHandle, NewOwner*);
+
+  static size_t WriteCallback(char* ptr, size_t size, size_t nmemb,
+                              void* userdata);
+  static size_t HeaderCallback(char* buffer, size_t size, size_t nitems,
+                               void* userdata);
+  static int ProgressCallback(void* clientp, curl_off_t dltotal,
+                              curl_off_t dlnow, curl_off_t ultotal,
+                              curl_off_t ulnow);
+  static size_t ReadCallback(char* buffer, size_t size, size_t nitems,
+                             void* userdata);
+  static void OnNextRequestBodyChunkRequested(evutil_socket_t, short,
+                                              void* userdata);
+
+  struct Data;
+
+  struct OnCancel {
+    void operator()() const;
+    Data* data;
+  };
+
+  friend class CurlHttpImpl;
+  friend class CurlHttpOperation;
+  friend class CurlHttpBodyGenerator;
+
+  std::unique_ptr<Data> d_;
+};
+
+class CurlHttpBodyGenerator : public HttpBodyGenerator<CurlHttpBodyGenerator> {
+ public:
+  CurlHttpBodyGenerator(CurlHandle handle, std::string_view initial_chunk);
+
+  CurlHttpBodyGenerator(const CurlHttpBodyGenerator&) = delete;
+  CurlHttpBodyGenerator(CurlHttpBodyGenerator&&) noexcept;
+  ~CurlHttpBodyGenerator();
+
+  CurlHttpBodyGenerator& operator=(const CurlHttpBodyGenerator&) = delete;
+  CurlHttpBodyGenerator& operator=(CurlHttpBodyGenerator&&) noexcept;
+
+  void Resume();
+
+ private:
+  static void OnChunkReady(evutil_socket_t, short, void* handle);
+  static void OnBodyReady(evutil_socket_t, short, void* handle);
+
+  friend class CurlHttpOperation;
+  friend class CurlHandle;
+  friend class CurlHttpImpl;
+
+  event chunk_ready_;
+  event body_ready_;
+  bool body_ready_fired_ = false;
+  int status_ = -1;
+  std::exception_ptr exception_ptr_;
+  std::string data_;
+  CurlHandle handle_;
+};
+
+class CurlHttpOperation {
+ public:
+  CurlHttpOperation(CURLM* http, event_base* event_loop, Request<>,
+                    const std::string* cache_path, stdx::stop_token);
+  CurlHttpOperation(const CurlHttpOperation&) = delete;
+  CurlHttpOperation(CurlHttpOperation&&) noexcept;
+  ~CurlHttpOperation();
+
+  CurlHttpOperation& operator=(const CurlHttpOperation&) = delete;
+  CurlHttpOperation& operator=(CurlHttpOperation&&) = delete;
+
+  bool await_ready();
+  void await_suspend(stdx::coroutine_handle<void> awaiting_coroutine);
+  Response<CurlHttpBodyGenerator> await_resume();
+
+ private:
+  static void OnHeadersReady(evutil_socket_t fd, short event, void* handle);
+
+  friend class CurlHttpImpl;
+  friend class CurlHandle;
+
+  stdx::coroutine_handle<void> awaiting_coroutine_;
+  std::exception_ptr exception_ptr_;
+  event headers_ready_;
+  bool headers_ready_event_posted_;
+  int status_ = -1;
+  std::vector<std::pair<std::string, std::string>> headers_;
+  std::string body_;
+  bool no_body_ = false;
+  CurlHandle handle_;
+};
+
+class CurlHttpImpl {
+ public:
+  CurlHttpImpl(event_base* event_loop, std::optional<std::string> cache_path);
+
+  CurlHttpImpl(CurlHttpImpl&&) = delete;
+  CurlHttpImpl& operator=(CurlHttpImpl&&) = delete;
+
+  ~CurlHttpImpl();
+
+  CurlHttpOperation Fetch(Request<> request,
+                          stdx::stop_token = stdx::stop_token()) const;
+
+ private:
+  static int SocketCallback(CURL* handle, curl_socket_t socket, int what,
+                            void* userp, void* socketp);
+  static int TimerCallback(CURLM* handle, long timeout_ms, void* userp);
+  static void SocketEvent(evutil_socket_t fd, short event, void* multi_handle);
+  static void TimeoutEvent(evutil_socket_t fd, short event, void* handle);
+  static void ProcessEvents(CURLM* handle);
+
+  friend class CurlHttpOperation;
+  friend class CurlHttpBodyGenerator;
+  friend class CurlHandle;
+
+  struct CurlMultiDeleter {
+    void operator()(CURLM* handle) const;
+  };
+
+  std::unique_ptr<CURLM, CurlMultiDeleter> curl_handle_;
+  event_base* event_loop_;
+  event timeout_event_;
+  std::optional<std::string> cache_path_;
+};
 
 struct CurlHandle::Data {
   template <typename Owner>
@@ -588,6 +731,37 @@ CurlHttpOperation CurlHttpImpl::Fetch(Request<> request,
                                       stdx::stop_token token) const {
   return {curl_handle_.get(), event_loop_, std::move(request),
           cache_path_ ? &*cache_path_ : nullptr, std::move(token)};
+}
+
+void CurlHttpImpl::CurlMultiDeleter::operator()(CURLM* handle) const {
+  if (handle) {
+    curl_multi_cleanup(handle);
+  }
+}
+
+Generator<std::string> ToBody(CurlHttpBodyGenerator d) {
+  FOR_CO_AWAIT(std::string & chunk, d) { co_yield std::move(chunk); }
+}
+
+}  // namespace
+
+struct CurlHttpBase::Impl {
+  CurlHttpImpl impl;
+};
+
+CurlHttpBase::CurlHttpBase(event_base* event_loop,
+                           std::optional<std::string> cache_path)
+    : d_(new Impl{{event_loop, cache_path}}) {}
+
+CurlHttpBase::~CurlHttpBase() = default;
+
+Task<Response<>> CurlHttpBase::Fetch(Request<> request,
+                                     stdx::stop_token stop_token) const {
+  auto response =
+      co_await d_->impl.Fetch(std::move(request), std::move(stop_token));
+  co_return Response<>{.status = response.status,
+                       .headers = std::move(response.headers),
+                       .body = ToBody(std::move(response.body))};
 }
 
 }  // namespace coro::http
--- src/coro/http/curl_http.h
+++ src/coro/http/curl_http.h
@@ -1,164 +1,29 @@
 #ifndef CORO_HTTP_SRC_CORO_HTTP_CURL_HTTP_H_
 #define CORO_HTTP_SRC_CORO_HTTP_CURL_HTTP_H_
 
-#include <curl/curl.h>
-#include <event2/event.h>
-#include <event2/event_struct.h>
-
 #include <variant>
 
 #include "coro/http/http.h"
-#include "coro/promise.h"
 #include "coro/stdx/stop_callback.h"
+
+struct event_base;
 
 namespace coro::http {
 
-class CurlHttpImpl;
-class CurlHttpOperation;
-class CurlHttpBodyGenerator;
-
-class CurlHandle {
+class CurlHttpBase {
  public:
-  CurlHandle(CurlHandle&&) = default;
-  CurlHandle& operator=(CurlHandle&&) = default;
+  CurlHttpBase(event_base* event_loop, std::optional<std::string> cache_path);
+  ~CurlHttpBase();
 
-  ~CurlHandle();
+  Task<Response<>> Fetch(Request<> request, stdx::stop_token stop_token) const;
 
  private:
-  template <typename Owner>
-  CurlHandle(CURLM* http, event_base* event_loop, Request<>,
-             const std::string* cache_path, stdx::stop_token, Owner*);
+  class Impl;
 
-  template <typename NewOwner>
-  CurlHandle(CurlHandle, NewOwner*);
-
-  static size_t WriteCallback(char* ptr, size_t size, size_t nmemb,
-                              void* userdata);
-  static size_t HeaderCallback(char* buffer, size_t size, size_t nitems,
-                               void* userdata);
-  static int ProgressCallback(void* clientp, curl_off_t dltotal,
-                              curl_off_t dlnow, curl_off_t ultotal,
-                              curl_off_t ulnow);
-  static size_t ReadCallback(char* buffer, size_t size, size_t nitems,
-                             void* userdata);
-  static void OnNextRequestBodyChunkRequested(evutil_socket_t, short,
-                                              void* userdata);
-
-  struct Data;
-
-  struct OnCancel {
-    void operator()() const;
-    Data* data;
-  };
-
-  friend class CurlHttpImpl;
-  friend class CurlHttpOperation;
-  friend class CurlHttpBodyGenerator;
-
-  std::unique_ptr<Data> d_;
+  std::unique_ptr<Impl> d_;
 };
 
-class CurlHttpBodyGenerator : public HttpBodyGenerator<CurlHttpBodyGenerator> {
- public:
-  CurlHttpBodyGenerator(CurlHandle handle, std::string_view initial_chunk);
-
-  CurlHttpBodyGenerator(const CurlHttpBodyGenerator&) = delete;
-  CurlHttpBodyGenerator(CurlHttpBodyGenerator&&) noexcept;
-  ~CurlHttpBodyGenerator();
-
-  CurlHttpBodyGenerator& operator=(const CurlHttpBodyGenerator&) = delete;
-  CurlHttpBodyGenerator& operator=(CurlHttpBodyGenerator&&) noexcept;
-
-  void Resume();
-
- private:
-  static void OnChunkReady(evutil_socket_t, short, void* handle);
-  static void OnBodyReady(evutil_socket_t, short, void* handle);
-
-  friend class CurlHttpOperation;
-  friend class CurlHandle;
-  friend class CurlHttpImpl;
-
-  event chunk_ready_;
-  event body_ready_;
-  bool body_ready_fired_ = false;
-  int status_ = -1;
-  std::exception_ptr exception_ptr_;
-  std::string data_;
-  CurlHandle handle_;
-};
-
-class CurlHttpOperation {
- public:
-  CurlHttpOperation(CURLM* http, event_base* event_loop, Request<>,
-                    const std::string* cache_path, stdx::stop_token);
-  CurlHttpOperation(const CurlHttpOperation&) = delete;
-  CurlHttpOperation(CurlHttpOperation&&) noexcept;
-  ~CurlHttpOperation();
-
-  CurlHttpOperation& operator=(const CurlHttpOperation&) = delete;
-  CurlHttpOperation& operator=(CurlHttpOperation&&) = delete;
-
-  bool await_ready();
-  void await_suspend(stdx::coroutine_handle<void> awaiting_coroutine);
-  Response<CurlHttpBodyGenerator> await_resume();
-
- private:
-  static void OnHeadersReady(evutil_socket_t fd, short event, void* handle);
-
-  friend class CurlHttpImpl;
-  friend class CurlHandle;
-
-  stdx::coroutine_handle<void> awaiting_coroutine_;
-  std::exception_ptr exception_ptr_;
-  event headers_ready_;
-  bool headers_ready_event_posted_;
-  int status_ = -1;
-  std::vector<std::pair<std::string, std::string>> headers_;
-  std::string body_;
-  bool no_body_ = false;
-  CurlHandle handle_;
-};
-
-class CurlHttpImpl {
- public:
-  CurlHttpImpl(event_base* event_loop, std::optional<std::string> cache_path);
-
-  CurlHttpImpl(CurlHttpImpl&&) = delete;
-  CurlHttpImpl& operator=(CurlHttpImpl&&) = delete;
-
-  ~CurlHttpImpl();
-
-  CurlHttpOperation Fetch(Request<> request,
-                          stdx::stop_token = stdx::stop_token()) const;
-
- private:
-  static int SocketCallback(CURL* handle, curl_socket_t socket, int what,
-                            void* userp, void* socketp);
-  static int TimerCallback(CURLM* handle, long timeout_ms, void* userp);
-  static void SocketEvent(evutil_socket_t fd, short event, void* multi_handle);
-  static void TimeoutEvent(evutil_socket_t fd, short event, void* handle);
-  static void ProcessEvents(CURLM* handle);
-
-  friend class CurlHttpOperation;
-  friend class CurlHttpBodyGenerator;
-  friend class CurlHandle;
-
-  struct CurlMultiDeleter {
-    void operator()(CURLM* handle) const {
-      if (handle) {
-        curl_multi_cleanup(handle);
-      }
-    }
-  };
-
-  std::unique_ptr<CURLM, CurlMultiDeleter> curl_handle_;
-  event_base* event_loop_;
-  event timeout_event_;
-  std::optional<std::string> cache_path_;
-};
-
-using CurlHttp = ToHttpClient<CurlHttpImpl>;
+using CurlHttp = ToHttpClient<CurlHttpBase>;
 
 }  // namespace coro::http
 

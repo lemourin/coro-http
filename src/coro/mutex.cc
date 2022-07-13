c6fbae5229ef0b1344269385c3fbd8e71383861c
Paweł Wegner <pawel.wegner95@gmail.com>
2022-07-13 22:34:01 +0200

    mutex: implement rw lock.

--- src/coro/mutex.cc
+++ src/coro/mutex.cc
@@ -6,13 +6,15 @@
 
 namespace coro {
 
+using ::coro::util::AtScopeExit;
+
 Mutex::Mutex() : locked_() {}
 
 Task<> Mutex::Lock() {
   if (locked_) {
     Promise<void> promise;
     queued_.emplace_back(&promise);
-    auto guard = util::AtScopeExit([&] {
+    auto guard = AtScopeExit([&] {
       queued_.erase(std::find(queued_.begin(), queued_.end(), &promise));
     });
     co_await promise;
@@ -51,5 +53,103 @@ Task<UniqueLock> UniqueLock::Create(Mutex* mutex) {
   co_await mutex->Lock();
   co_return UniqueLock(mutex);
 }
+
+ReadWriteMutex::ReadWriteMutex() : reader_count_(), writer_count_() {}
+
+Task<> ReadWriteMutex::ReadLock() {
+  if (writer_count_ > 0) {
+    Promise<void> promise;
+    queued_readers_.emplace_back(&promise);
+    auto guard = AtScopeExit([&] {
+      queued_readers_.erase(
+          std::find(queued_readers_.begin(), queued_readers_.end(), &promise));
+    });
+    co_await promise;
+  }
+  reader_count_++;
+}
+
+void ReadWriteMutex::ReadUnlock() {
+  reader_count_--;
+  if (reader_count_ == 0 && !queued_writers_.empty()) {
+    queued_writers_.front()->SetValue();
+  }
+}
+
+Task<> ReadWriteMutex::WriteLock() {
+  if (reader_count_ > 0) {
+    Promise<void> promise;
+    queued_writers_.emplace_back(&promise);
+    auto guard = AtScopeExit([&] {
+      queued_writers_.erase(
+          std::find(queued_writers_.begin(), queued_writers_.end(), &promise));
+    });
+    co_await promise;
+  }
+  writer_count_++;
+}
+
+void ReadWriteMutex::WriteUnlock() {
+  writer_count_--;
+  if (writer_count_ == 0) {
+    if (!queued_writers_.empty()) {
+      queued_writers_.front()->SetValue();
+    } else {
+      while (writer_count_ == 0 && !queued_readers_.empty()) {
+        queued_readers_.front()->SetValue();
+      }
+    }
+  }
+}
+
+ReadLock::ReadLock(ReadWriteMutex* mutex) : mutex_(mutex) {}
+
+ReadLock::ReadLock(ReadLock&& other) noexcept
+    : mutex_(std::exchange(other.mutex_, nullptr)) {}
+
+ReadLock& ReadLock::operator=(ReadLock&& other) noexcept {
+  mutex_ = std::exchange(other.mutex_, nullptr);
+  return *this;
+}
+
+Task<ReadLock> ReadLock::Create(ReadWriteMutex* mutex) {
+  co_await mutex->ReadLock();
+  co_return ReadLock(mutex);
+}
+
+ReadLock::~ReadLock() {
+  if (mutex_) {
+    try {
+      mutex_->ReadUnlock();
+    } catch (...) {
+      std::terminate();
+    }
+  }
+}
+
+WriteLock::WriteLock(WriteLock&& other) noexcept
+    : mutex_(std::exchange(other.mutex_, nullptr)) {}
+
+WriteLock& WriteLock::operator=(WriteLock&& other) noexcept {
+  mutex_ = std::exchange(other.mutex_, nullptr);
+  return *this;
+}
+
+WriteLock::~WriteLock() {
+  if (mutex_) {
+    try {
+      mutex_->WriteUnlock();
+    } catch (...) {
+      std::terminate();
+    }
+  }
+}
+
+Task<WriteLock> WriteLock::Create(ReadWriteMutex* mutex) {
+  co_await mutex->WriteLock();
+  co_return WriteLock(mutex);
+}
+
+WriteLock::WriteLock(ReadWriteMutex* mutex) : mutex_(mutex) {}
 
 }  // namespace coro
--- src/coro/mutex.h
+++ src/coro/mutex.h
@@ -36,9 +36,64 @@ class UniqueLock {
   static Task<UniqueLock> Create(Mutex*);
 
  private:
-  UniqueLock(Mutex*);
+  explicit UniqueLock(Mutex*);
 
   Mutex* mutex_;
+};
+
+class ReadWriteMutex {
+ public:
+  ReadWriteMutex();
+  ReadWriteMutex(const ReadWriteMutex&) = delete;
+  ReadWriteMutex(ReadWriteMutex&&) = delete;
+  ReadWriteMutex& operator=(const ReadWriteMutex&) = delete;
+  ReadWriteMutex& operator=(ReadWriteMutex&&) = delete;
+
+  Task<> ReadLock();
+  void ReadUnlock();
+
+  Task<> WriteLock();
+  void WriteUnlock();
+
+ private:
+  int reader_count_;
+  std::vector<Promise<void>*> queued_readers_;
+  int writer_count_;
+  std::vector<Promise<void>*> queued_writers_;
+};
+
+class ReadLock {
+ public:
+  ReadLock(const ReadLock&) = delete;
+  ReadLock(ReadLock&&) noexcept;
+  ReadLock& operator=(const ReadLock&) = delete;
+  ReadLock& operator=(ReadLock&&) noexcept;
+
+  ~ReadLock();
+
+  static Task<ReadLock> Create(ReadWriteMutex*);
+
+ private:
+  explicit ReadLock(ReadWriteMutex* mutex);
+
+  ReadWriteMutex* mutex_;
+};
+
+class WriteLock {
+ public:
+  WriteLock(const WriteLock&) = delete;
+  WriteLock(WriteLock&&) noexcept;
+  WriteLock& operator=(const WriteLock&) = delete;
+  WriteLock& operator=(WriteLock&&) noexcept;
+
+  ~WriteLock();
+
+  static Task<WriteLock> Create(ReadWriteMutex*);
+
+ private:
+  explicit WriteLock(ReadWriteMutex* mutex);
+
+  ReadWriteMutex* mutex_;
 };
 
 }  // namespace coro
